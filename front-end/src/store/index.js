ff9b5c9900b993fcc3fc1c5f5b11512273fb540b
Hwangbo Jaeyun <hwangbo0319@gmail.com>
2024-08-02 09:32:17 +0900

    🛠️ 코드 정리 및 색 변경

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -91,41 +91,22 @@ const store = createStore({
     }
   },
   getters: {
-    allData: state => state.allData,
     id: state => state.id,
+    allData: state => state.allData,
+    windDirectionData: state => state.windDirectionData,
+
+    // 라인 차트
     tempData: state => ({
       labels: state.Chartlabels,
       datasets: [{
-        label: 'Temperature',
-        data: state.tempData,
-        borderWidth: 3,
-        backgroundColor: '#0000FF',
-        borderColor: '#FFFFFF',
-        tension: 0.3,
-        pointRadius: 0
-      }]
-    }),
-    presData: state => ({
-      labels: state.Chartlabels,
-      datasets: [{
-        label: 'Pressure',
-        data: state.presData,
-        borderWidth: 3,
-        backgroundColor: '#0000FF',
-        borderColor: '#FFFFFF',
-        tension: 0.3,
-        pointRadius: 0
-      }]
-    }),
-    humidityData: state => ({
-      labels: ['Humidity', '.'],
-      datasets: [{
-        borderWidth: 0,
-        backgroundColor: ["#FFFFFF", "#151515"],
-        hoverBackgroundColor: ["#FFFFFF", "#000000"],
-        hoverBorderColor: ["#FFFFFF", "#000000"],
-        hoverBorderWidth: 0,
-        data: [state.humidityData, 100 - state.humidityData]
+        label: 'Temperature',   //데이터의 라벨
+        data: state.tempData,   //리스트형
+        borderWidth: 3,   //선의 굵기
+        backgroundColor: 'rgb(255,255,255, 0.15)',   //FILL 시 채워지는 색
+        borderColor: '#FFFFFF',   //라인의 색
+        tension: 0.3,   //선의 텐션
+        pointRadius: 3,   //특정 데이터 포인터의 크기
+        fill: true   //Y축 0부터 데이터 까지 채워넣을것인지
       }]
     }),
     windSpeedData: state => ({
@@ -134,13 +115,40 @@ const store = createStore({
         label: 'Wind Speed',
         data: state.windSpeedData,
         borderWidth: 3,
-        backgroundColor: '#0000FF',
+        backgroundColor: 'rgb(255,255,255, 0.15)',
+        borderColor: '#FFFFFF',
+        tension: 0.3,
+        pointRadius: 3,
+        fill: true
+      }]
+    }),
+
+    // 막대 차트 (라인 차트와 설정방법 같음)
+    presData: state => ({
+      labels: state.Chartlabels,
+      datasets: [{
+        label: 'Pressure',
+        data: state.presData,
+        borderWidth: 0,
+        backgroundColor: '#FFFFFF',
         borderColor: '#FFFFFF',
         tension: 0.3,
         pointRadius: 0
       }]
     }),
-    windDirectionData: state => state.windDirectionData
+
+    //도넛 차트
+    humidityData: state => ({
+      labels: ['Humidity', '.'],
+      datasets: [{
+        borderWidth: 2,
+        backgroundColor: ["#FFFFFF", "#151515"],
+        hoverBackgroundColor: ["#FFFFFF", "#000000"],
+        hoverBorderColor: ["#FFFFFF", "#000000"],
+        hoverBorderWidth: 0,
+        data: [state.humidityData, 100 - state.humidityData]
+      }]
+    }),
   },
   plugins: [vuexLocal.plugin]
 });
