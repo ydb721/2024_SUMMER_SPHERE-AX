5d8aa94c11da88c72ca8f0e792ccd2b77f57f518
Hwangbo Jaeyun <hwangbo0319@gmail.com>
2024-08-05 09:32:51 +0900

    🛠️ RawData 디자인 변경 완료

--- front-end/src/views/RawData.vue
+++ front-end/src/views/RawData.vue
@@ -1,13 +1,5 @@
 <template>
-<div class="desktop-2">
-  <div class="frame-9">
-    <img class="line-1" src="../assets/rawdata-img/line-10.svg" />
-    <div class="real-time-weather-data">
-      Real-Time
-      <br />
-      Weather Data
-    </div>
-  </div>
+  <div class="main">
   <div class="frame-21">
     <div class="temp">
       <div class="frame-13">
@@ -16,7 +8,7 @@
           <div class="temperature2">Temperature</div>
         </div>
         <div class="temp-data">
-          <div class="_100-c">100 °C</div>
+          <div class="_100-c">{{ displayedData.allData.Temperature }} °C</div>
         </div>
       </div>
     </div>
@@ -27,7 +19,7 @@
           <div class="humidity">Humidity</div>
         </div>
         <div class="hum-data">
-          <div class="_70">70 %</div>
+          <div class="_70">{{ displayedData.allData.Humidity }} %</div>
         </div>
       </div>
     </div>
@@ -38,7 +30,7 @@
           <div class="pressure">Pressure</div>
         </div>
         <div class="pres-data">
-          <div class="_998-7-h-pa">998.7 hPa</div>
+          <div class="_998-7-h-pa">{{ displayedData.allData.Pressure }} hPa</div>
         </div>
       </div>
     </div>
@@ -49,7 +41,7 @@
           <div class="wind-direction">Wind Direction</div>
         </div>
         <div class="ws-data">
-          <div class="dir2">동남동 Dir</div>
+          <div class="dir2">{{ displayedData.windDirectionData }} 향</div>
         </div>
       </div>
     </div>
@@ -60,13 +52,12 @@
           <div class="wind-speed2">Wind Speed</div>
         </div>
         <div class="ws-data">
-          <div class="_15-7-m-s">15.7 m / s</div>
+          <div class="_15-7-m-s">{{ displayedData.allData.WindSpeed }} m / s</div>
         </div>
       </div>
     </div>
   </div>
-</div>
-
+  </div>
 </template>
 
 <script>
@@ -98,46 +89,14 @@ export default {
 <style>
 @import '../vars.css';
 
-.desktop-2,
-.desktop-2 * {
-  box-sizing: border-box;
-}
-.desktop-2 {
-  background: #000000;
-  position: relative;
-  height: 80vh;
-  overflow: hidden;
-}
-.frame-9 {
+.main {
   display: flex;
-  flex-direction: row;
-  gap: 28px;
-  row-gap: 0px;
-  align-items: flex-start;
-  justify-content: flex-end;
-  width: 250px;
-  position: absolute;
-  left: calc(50% - 695px);
-  top: 17px;
+  justify-content: center; /* 수평 중앙 정렬 */
+  align-items: center;     /* 수직 중앙 정렬 */
+  height: 80vh;
+  margin: 0;
 }
-.line-1 {
-  flex-shrink: 0;
-  width: 411px;
-  height: 0px;
-  position: relative;
-  overflow: visible;
-}
-.real-time-weather-data {
-  color: #ffffff;
-  text-align: left;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: absolute;
-  right: 28px;
-  left: 0px;
-  top: 0px;
-}
+
 .frame-21 {
   display: flex;
   flex-direction: row;
@@ -149,12 +108,12 @@ export default {
   align-content: flex-start;
   width: 1098px;
   position: absolute;
-  left: calc(50% - 403px);
-  top: 39px;
+  top: 20vh;
 }
-.temp {
+
+.temp, .hum, .press, .dir, .wind-speed {
   background: rgba(217, 217, 217, 0);
-  padding: 27px 35px 27px 35px;
+  padding: 27px;
   display: flex;
   flex-direction: column;
   gap: 10px;
@@ -166,7 +125,8 @@ export default {
   height: 164px;
   position: relative;
 }
-.frame-13 {
+
+.frame-13, .frame-16, .frame-18, .frame-20, .frame-19 {
   display: flex;
   flex-direction: column;
   gap: 1px;
@@ -174,10 +134,10 @@ export default {
   align-items: center;
   justify-content: flex-start;
   flex-shrink: 0;
-  width: 267px;
   position: relative;
 }
-.frame {
+
+.frame, .frame-14, .frame-17, .frame-172, .frame-173 {
   display: flex;
   flex-direction: row;
   gap: 10px;
@@ -185,312 +145,44 @@ export default {
   align-items: flex-start;
   justify-content: center;
   flex-shrink: 0;
-  width: 267px;
   position: relative;
 }
-.temperature {
+
+.temperature, .hygrometer, .pressure-gauge, .north-direction, .wind-speed-18-22 {
   flex-shrink: 0;
   width: 47px;
   height: 47px;
   position: relative;
   object-fit: cover;
 }
-.temperature2 {
+
+.temperature2, .humidity, .pressure, .wind-direction, .wind-speed2 {
   color: #ffffff;
   text-align: left;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-.temp-data {
-  padding: 10px;
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: center;
-  flex-shrink: 0;
-  position: relative;
-}
-._100-c {
-  color: #ffffff;
-  text-align: center;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-.hum {
-  background: rgba(217, 217, 217, 0);
-  padding: 27px 64px 27px 64px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: center;
-  flex-shrink: 0;
-  width: 338px;
-  height: 164px;
-  position: relative;
-}
-.frame-16 {
-  display: flex;
-  flex-direction: column;
-  gap: 1px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  width: 209px;
-  position: relative;
-}
-.frame-14 {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: flex-start;
-  justify-content: center;
-  align-self: stretch;
-  flex-shrink: 0;
-  position: relative;
-}
-.hygrometer {
-  flex-shrink: 0;
-  width: 47px;
-  height: 47px;
-  position: relative;
-  object-fit: cover;
-}
-.humidity {
-  color: #ffffff;
-  text-align: left;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-.hum-data {
-  padding: 10px;
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: center;
-  flex-shrink: 0;
-  position: relative;
-}
-._70 {
-  color: #ffffff;
-  text-align: center;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-.press {
-  background: rgba(217, 217, 217, 0);
-  padding: 27px 67px 27px 67px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: flex-start;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  width: 338px;
-  height: 164px;
-  position: relative;
-}
-.frame-18 {
-  display: flex;
-  flex-direction: column;
-  gap: 1px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  width: 204px;
-  position: relative;
-}
-.frame-17 {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: flex-start;
-  justify-content: center;
-  align-self: stretch;
-  flex-shrink: 0;
-  position: relative;
-}
-.pressure-gauge {
-  flex-shrink: 0;
-  width: 47px;
-  height: 47px;
-  position: relative;
-  object-fit: cover;
-}
-.pressure {
-  color: #ffffff;
-  text-align: left;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-.pres-data {
-  padding: 10px;
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: center;
-  flex-shrink: 0;
-  position: relative;
-}
-._998-7-h-pa {
-  color: #ffffff;
-  text-align: center;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-.dir {
-  background: rgba(217, 217, 217, 0);
-  padding: 27px 16px 27px 16px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  width: 338px;
-  height: 164px;
-  position: relative;
-}
-.frame-20 {
-  display: flex;
-  flex-direction: column;
-  gap: 1px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  width: 305px;
-  position: relative;
-}
-.frame-172 {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: flex-start;
-  justify-content: center;
-  flex-shrink: 0;
-  width: 300px;
-  position: relative;
-}
-.north-direction {
-  flex-shrink: 0;
-  width: 47px;
-  height: 47px;
-  position: relative;
-  object-fit: cover;
-}
-.wind-direction {
-  color: #ffffff;
-  text-align: left;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-.ws-data {
-  padding: 10px;
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: center;
-  flex-shrink: 0;
-  position: relative;
-}
-.dir2 {
-  color: #ffffff;
-  text-align: center;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-.wind-speed {
-  background: rgba(217, 217, 217, 0);
-  padding: 27px 67px 27px 67px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  width: 338px;
-  height: 164px;
-  position: relative;
-}
-.frame-19 {
-  display: flex;
-  flex-direction: column;
-  gap: 1px;
-  row-gap: 0px;
-  align-items: center;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  width: 250px;
-  position: relative;
-}
-.frame-173 {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  row-gap: 0px;
-  align-items: flex-start;
-  justify-content: center;
-  flex-shrink: 0;
-  width: 250px;
-  position: relative;
-}
-.wind-speed-18-22 {
-  flex-shrink: 0;
-  width: 47px;
-  height: 47px;
-  position: relative;
-  object-fit: cover;
-}
-.wind-speed2 {
-  color: #ffffff;
-  text-align: left;
-  font-family: "Arial-Black", sans-serif;
-  font-size: 30px;
-  font-weight: 900;
-  position: relative;
-}
-._15-7-m-s {
-  color: #ffffff;
-  text-align: center;
   font-family: "Arial-Black", sans-serif;
   font-size: 30px;
   font-weight: 900;
   position: relative;
 }
 
+.temp-data, .hum-data, .pres-data, .ws-data {
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  row-gap: 0px;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  position: relative;
+}
 
+._100-c, ._70, ._998-7-h-pa, .dir2, ._15-7-m-s {
+  color: #ffffff;
+  text-align: center;
+  font-family: "Arial-Black", sans-serif;
+  font-size: 30px;
+  font-weight: 900;
+  position: relative;
+}
 </style>
